487f37db0eb99cb6753c17b84ec93619a4b94c60
Sameerjohri <Sameerjohri>
2025-09-03 11:40:35 +0530

    new

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask,render_template
+import requests
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def home():
+    url="https://newsapi.org/v2/everything?q=bitcoin&apiKey=d795e8b8466949dbbdcd436eb8d2a310"
+    
+    r = requests.get(url).json()
+
+    news = {
+        'articles' : r['articles']
+    }
+    return render_template('index.html',allNews=news)
